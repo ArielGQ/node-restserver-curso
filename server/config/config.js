e2bc077576cce52f17c4f82c4c0d37b4187fa22a
ArielGQ <aquintana@focasoftware.com>
2020-10-17 10:33:37 -0300

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,8 @@ let urlDB;
 if (process.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://mongodb:RHUUWoNb2fh5K0Xc@cluster0.gejz2.mongodb.net/cafe?retryWrites=true&w=majority'
+    //urlDB = 'mongodb+srv://mongodb:RHUUWoNb2fh5K0Xc@cluster0.gejz2.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 // mongo "mongodb+srv://cluster0.gejz2.mongodb.net/<dbname>" --username mongodb
